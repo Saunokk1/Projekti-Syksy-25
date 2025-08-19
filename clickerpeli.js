fdc62fb6cffce347983bc11b0d3a7d4c2c2ecd2f
Cocksadsa <eemil.luukkainen@gmail.com>
2025-08-19 16:18:40 +0300

    clicker

--- clickerpeli.js
+++ clickerpeli.js
@@ -1,4 +1,57 @@
- document.addEventListener('DOMContentLoaded', function() {
+let pisteet = 0;
+let pisteetPerClick = 1;
+let ppchinta = 15;
+let pisteetPerSecond = 0;
+let ppshinta = 75;
+
+const pisteetElement = document.getElementById('pisteet');
+const pisteetPerClickElement = document.getElementById('pisteetPerClick');
+const ppchintaElement = document.getElementById("ppchinta");
+const pisteetPerSecondElement = document.getElementById('pisteetPerSecond');
+const ppshintaElement = document.getElementById("ppshinta");
+
+function paivitaDisplay() {
+    pisteetElement.textContent = pisteet;
+    pisteetPerClickElement.textContent = pisteetPerClick;
+    ppchintaElement.textContent = ppchinta;
+    pisteetPerSecondElement.textContent = pisteetPerSecond;
+    ppshintaElement.textContent = ppshinta;
+}
+
+function lisääpiste() {
+    pisteet += pisteetPerClick;
+    paivitaDisplay();
+}
+
+function ostappc() {
+    if (pisteet >= ppchinta) {
+        pisteet -= ppchinta;
+        pisteetPerClick++;
+        ppchinta *= 2;
+        paivitaDisplay();
+    }   else {
+        alert("Ei tarpeeksi pisteitä!");
+    }
+}
+
+function ostapps() {
+    if (pisteet >= ppshinta) {
+        pisteet -= ppshinta;
+        pisteetPerSecond++;
+        ppshinta *= 2.2;
+        setInterval(function() {
+            pisteet += pisteetPerSecond;
+            paivitaDisplay();
+        }, 1000);
+        paivitaDisplay();
+    }   else {
+        alert("Ei tarpeeksi pisteitä!");
+    }
+}
+
+paivitaDisplay();
+
+document.addEventListener('DOMContentLoaded', function() {
             const menuButton = document.getElementById('menuButton');
             const closeButton = document.getElementById('closeButton');
             const slideMenu = document.getElementById('slideMenu');
@@ -26,3 +79,5 @@
                 }
             });
         });
+
+        
